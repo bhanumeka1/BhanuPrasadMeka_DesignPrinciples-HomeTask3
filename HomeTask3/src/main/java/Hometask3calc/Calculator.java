8c989d7e5e719a8ba94f7ff867d4d63af0adfd54
bhanumeka1 <59662390+bhanumeka1@users.noreply.github.com>
2020-02-10 23:38:32 +0530

    Add files via upload

---
+++ HomeTask3/src/main/java/Hometask3calc/Calculator.java
@@ -0,0 +1,31 @@
+package Hometask3calc;
+
+public class Calculator{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int sub(int a,int b)
+	{
+		return a-b;
+
+	}
+	public int mul(int a,int b)
+	{
+		return a*b;
+	}
+	public int div(int a,int b)
+	{
+		return a/b;
+	}
+	public int pow(int a,int b)
+	{
+		int p = 1;
+		for(int i=1;i<=b;i++)
+		{
+			p*=a;
+		}
+		return p;
+	}
+}
+
